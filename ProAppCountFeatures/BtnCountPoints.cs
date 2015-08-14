a6e46aaa8f3e5e333e552d33bb66ba456f87a6c2
David Wilton <dave_wilton@msn.com>
2015-08-14 05:32:13 +0100

    First commit

---
+++ ProAppCountFeatures/BtnCountPoints.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using ArcGIS.Desktop.Framework.Contracts;
+
+
+namespace ProAppCountFeatures
+{
+    internal class BtnCountPoints : Button
+    {
+        protected async override void OnClick()
+        {
+            var points = new SampleQueuedTask();
+            var result = await points.CountFeaturesAsyc();
+            ArcGIS.Desktop.Framework.Dialogs.MessageBox.Show(result.ToString(CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ ProAppCountFeatures/sampleQueuedTask.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ArcGIS.Core.Data;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Mapping;
+
+namespace ProAppCountFeatures
+{
+    class SampleQueuedTask
+    {
+
+        public Task<int> CountFeaturesAsyc()
+        {
+            Func<int> methodToCall = CountFeatures;
+            return QueuedTask.Run(methodToCall);
+
+        }
+
+        private static int CountFeatures()
+        {
+            // You can now access fine grained API's and
+            // edit here whilst debugging
+
+            //get the layer
+            var activeMap = MapView.Active.Map;
+
+            //get the first feature layer
+            var featureLayer = activeMap.GetLayersAsFlattenedList().OfType<FeatureLayer>().FirstOrDefault();
+            if (featureLayer == null) throw new Exception();
+
+            //get the underlying feature class for each layer
+            var featureClass = featureLayer.GetTable() as FeatureClass;
+            if (featureClass == null) throw new Exception("No feature class in map");
+
+            var featCursor = featureClass.Search(null, false);
+            //loop through the features and count
+            var i = 0;
+            while (featCursor.MoveNext())
+            {
+                i++;
+            }
+            return i;
+        }
+
+    }
+}
